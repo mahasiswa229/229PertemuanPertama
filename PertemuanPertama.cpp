3a28475e1c13f4c44054d2ac2e5bced4b736c064
mahasiswa229 <mafaqih1945@gmail.com>
2024-02-27 16:28:49 +0800

    Add project files.

---
+++ PertemuanPertama.cpp
@@ -0,0 +1,26 @@
+// PertemuanPertama.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Begin
+    //numeric nAlas ,nTinggi ,nLuas
+    //Display 'Masukan Alas = '
+    //Accept nAlas
+    // Display 'Masukan Tinggi = '
+    // Accept nTinggi
+    // Compute nLuas = nAlas*nTinggi/2
+    //Display 'Luasnya =' + nLuas
+    //End
+    
+    float nAlas, nTinggi, nLuas;
+    cout << "Masukan Alas =";
+    cin >> nAlas;
+    cout << "Masukan Tinggi =";
+    cin >> nTinggi;
+    nLuas = nAlas * nTinggi / 2;
+    cout << "Luasnya = " << nLuas;
+
+}
